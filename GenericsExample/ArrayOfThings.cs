482334866231c71eb6188416cc7267bfed5103ce
Patryk Bielecki <p.patrykbielecki@gmail.com>
2018-03-26 18:13:49 +0200

    Generics example

---
+++ GenericsExample/ArrayOfThings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    public class ArrayOfThings<T>
+    {
+        private readonly T[] _array;
+
+        public ArrayOfThings(int size)
+        {
+            _array = new T[size];
+        }
+        public T Get(int index)
+        {
+            return _array[index];
+        }
+        public void Set(int index, T value)
+        {
+            _array[index] = value;
+        }
+    }
+}
---
+++ GenericsExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var arrayOfInt = new ArrayOfThings<int>(10);
+            var arrayOfString = new ArrayOfThings<string>(10);
+
+            for (var i = 0; i < 10; i++)
+            {
+                arrayOfInt.Set(i, i);
+                arrayOfString.Set(i, $"string number {i}.");
+            }
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(arrayOfInt.Get(i));
+                Console.WriteLine(arrayOfString.Get(i));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
